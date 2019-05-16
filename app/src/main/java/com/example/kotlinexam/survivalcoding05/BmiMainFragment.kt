ca51b0f2b3f2c1a8f0d004583770659966e36bb4
Junsuk Oh <a811219@gmail.com>
2019-05-16 12:28:32 +0900

    anko 제거, android-ktx 적용하여 bundleOf 사용

--- app/src/main/java/com/example/kotlinexam/survivalcoding05/BmiMainFragment.kt
+++ app/src/main/java/com/example/kotlinexam/survivalcoding05/BmiMainFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.example.kotlinexam.R
@@ -14,20 +15,23 @@ import kotlinx.android.synthetic.main.fragment_bmi_main.*
 class BmiMainFragment : Fragment() {
 
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_bmi_main, container, false)
 
         val button = view.findViewById<Button>(R.id.result_button)
         button.setOnClickListener {
-            val bundle = Bundle()
-            bundle.putDouble("height", height_edit.text.toString().toDouble())
-            bundle.putDouble("weight", weight_edit.text.toString().toDouble())
+            val bundle = bundleOf(
+                "height" to height_edit.text.toString().toDouble(),
+                "weight" to weight_edit.text.toString().toDouble()
+            )
 
             it.findNavController()
-                    .navigate(R.id.action_bmiMainFragment_to_bmiResultFragment
-                            , bundle)
+                .navigate(
+                    R.id.action_bmiMainFragment_to_bmiResultFragment
+                    , bundle
+                )
         }
 
         return view
