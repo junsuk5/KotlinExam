79b8617f53e0a8b923d9906c80e80be533d38b8c
Junsuk Oh <a811219@gmail.com>
2019-05-17 10:20:09 +0900

    비만도 계산

--- app/src/main/java/com/example/kotlinexam/survivalcoding05/BmiResultFragment.kt
+++ app/src/main/java/com/example/kotlinexam/survivalcoding05/BmiResultFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.example.kotlinexam.R
+import kotlinx.android.synthetic.main.fragment_bmi_result.*
 
 class BmiResultFragment : Fragment() {
     val args: BmiResultFragmentArgs by navArgs()
@@ -23,6 +24,17 @@ class BmiResultFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val bmi = args.weight / Math.pow(args.height / 100.0, 2.0)
+
+        when {
+            bmi >= 35 -> result_text.text = "고도 비만"
+            bmi >= 30 -> result_text.text = "2단계 비만"
+            bmi >= 25 -> result_text.text = "1단계 비만"
+            bmi >= 23 -> result_text.text = "과체중"
+            bmi >= 18.5 -> result_text.text = "정상"
+            else -> result_text.text = "저체중"
+        }
 
         Log.d("BmiResultFragment", "${args.height}, ${args.weight}")
     }
