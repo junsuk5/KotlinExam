5733c2f1e0576479324cbb83451579b0d5383c68
Junsuk Oh <a811219@gmail.com>
2019-05-28 10:35:00 +0900

    alert 확장함수

--- app/src/main/java/com/example/kotlinexam/MyExtentions.kt
+++ app/src/main/java/com/example/kotlinexam/MyExtentions.kt
@@ -1,8 +1,10 @@
 package com.example.kotlinexam
 
 import android.content.Context
+import android.content.DialogInterface
 import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 
 fun Context.toast(text: String) {
@@ -15,4 +17,40 @@ fun Fragment.toast(text: String) {
 
 fun Any.logd(text: String) {
     Log.d(this::class.java.simpleName, text)
+}
+
+fun Context.alert(
+    message: String,
+    title: String,
+    init: AlertDialogBuilder.() -> Unit
+) {
+    val dialog = AlertDialogBuilder(this, title, message)
+    init.invoke(dialog)
+    dialog.show()
+}
+
+class AlertDialogBuilder(context: Context, title: String, message: String) {
+    private val builder: AlertDialog.Builder by lazy {
+        AlertDialog.Builder(context)
+            .setTitle(title)
+            .setMessage(message)
+    }
+
+    fun positiveButton(
+        text: String, callback: DialogInterface.() -> Unit
+    ) {
+        builder.setPositiveButton(text) { dialog, _ ->
+            callback.invoke(dialog)
+        }
+    }
+
+    fun negativeButton(text: String, callback: DialogInterface.() -> Unit) {
+        builder.setNegativeButton(text) { dialog, _ ->
+            callback.invoke(dialog)
+        }
+    }
+
+    fun show() {
+        builder.show()
+    }
 }
