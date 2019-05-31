235c132a44954e1677ae5d2ebb5dd2fe733ed6da
Junsuk Oh <a811219@gmail.com>
2019-05-31 14:31:04 +0900

    채팅 완료

---
+++ app/src/main/java/com/example/kotlinexam/item12/ChatClient.kt
@@ -0,0 +1,105 @@
+package com.example.kotlinexam.item12
+
+import com.example.kotlinexam.item12.models.MsgInfo
+import com.google.gson.Gson
+import java.io.DataInputStream
+import java.io.DataOutputStream
+import java.io.IOException
+import java.net.Socket
+import java.net.UnknownHostException
+
+class ChatClient(
+    private val nickName: String,
+    private val onReadMessage: (MsgInfo) -> Unit
+) {
+
+    private var mSocket: Socket? = null
+    private var mName: String? = null
+
+    lateinit var writer: ClientWrite
+
+    fun connect() {
+        try {
+            mSocket = Socket(SERVER_HOST, SERVER_PORT)
+            val clientRead = ClientRead()
+            clientRead.start()
+
+            writer = ClientWrite(nickName)
+        } catch (e: UnknownHostException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
+    internal inner class ClientRead : Thread() {
+        private var mInputStream: DataInputStream? = null
+
+        override fun run() {
+            try {
+                mInputStream = DataInputStream(mSocket!!.getInputStream())
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+            super.run()
+            try {
+                // 계속 듣기만
+                while (mInputStream != null) {
+                    // json 파싱
+                    val json = mInputStream!!.readUTF()
+                    try {
+                        val msgInfo = Gson().fromJson(json, MsgInfo::class.java)
+                        println(json)
+                        onReadMessage.invoke(msgInfo)
+                    } catch (e: Exception) {
+                        println(e.localizedMessage)
+                    }
+
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } finally {
+                // 접속 종료시
+                mSocket = null
+            }
+        }
+    }
+
+    inner class ClientWrite(nickName: String) {
+
+        private var mOutputStream: DataOutputStream? = null
+
+        init {
+            try {
+                mName = nickName
+                mOutputStream = DataOutputStream(mSocket!!.getOutputStream())
+                mOutputStream!!.writeUTF(nickName)
+                mOutputStream!!.flush()
+                println("id : " + nickName + "접속 완료")
+            } catch (e: IOException) {
+                e.printStackTrace()
+                println("writeUTF IOException")
+            }
+
+        }
+
+        fun write(message: String) {
+
+            try {
+                val msgInfo = MsgInfo(nickName, message, System.currentTimeMillis())
+                mOutputStream!!.writeUTF(Gson().toJson(msgInfo))
+                mOutputStream!!.flush()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+
+        }
+    }
+
+    companion object {
+        private val SERVER_HOST = "192.168.0.222"
+        private val SERVER_PORT = 5000
+    }
+}
--- app/src/main/java/com/example/kotlinexam/item12/ui/ChatFragment.kt
+++ app/src/main/java/com/example/kotlinexam/item12/ui/ChatFragment.kt
@@ -7,9 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlinexam.R
+import com.example.kotlinexam.databinding.ItemChatMeBinding
+import com.example.kotlinexam.databinding.ItemChatYouBinding
+import com.example.kotlinexam.item12.ChatClient
+import com.example.kotlinexam.item12.models.MsgInfo
+import com.example.kotlinexam.logd
+import kotlinx.android.synthetic.main.fragment_chat.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class ChatFragment : Fragment() {
+class ChatFragment : Fragment(), CoroutineScope by CoroutineScope(Dispatchers.IO) {
     val args: ChatFragmentArgs by navArgs()
 
     override fun onCreateView(
@@ -23,6 +34,85 @@ class ChatFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val adapter = ChatAdapter(args.nickName)
+        recycler_view.layoutManager = LinearLayoutManager(requireContext())
+        recycler_view.adapter = adapter
 
+        val client = ChatClient(args.nickName) { msgInfo ->
+            launch(Dispatchers.Main) {
+                adapter.items.add(msgInfo)
+                adapter.notifyItemInserted(adapter.items.size - 1)
+            }
+            logd(msgInfo.toString())
+        }
+
+        launch {
+            client.connect()
+        }
+
+        send_button.setOnClickListener {
+
+            launch {
+                client.writer.write(chat_edit.text.toString())
+                launch(Dispatchers.Main) {
+                    chat_edit.setText("")
+                }
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+}
+
+class ChatAdapter(val nickName: String) :
+    RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    companion object {
+        const val ME_TYPE = 0
+        const val YOU_TYPE = 1
+    }
+
+    var items = arrayListOf<MsgInfo>()
+
+    interface MyHolder
+
+    class ChatMeHolder(val binding: ItemChatMeBinding) : RecyclerView.ViewHolder(binding.root), MyHolder
+
+    class ChatYouHolder(val binding: ItemChatYouBinding) : RecyclerView.ViewHolder(binding.root), MyHolder
+
+    override fun getItemViewType(position: Int): Int {
+        if (items[position].nickName == nickName) {
+            return ME_TYPE
+        }
+        return YOU_TYPE
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ME_TYPE -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_chat_me, parent, false)
+                val viewHolder = ChatMeHolder(ItemChatMeBinding.bind(view))
+                viewHolder
+            }
+            else -> {
+                val view = LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_chat_you, parent, false)
+                val viewHolder = ChatYouHolder(ItemChatYouBinding.bind(view))
+                viewHolder
+            }
+        }
+    }
+
+    override fun getItemCount() = items.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        if (holder is ChatMeHolder) {
+            holder.binding.info = items[position]
+        } else if (holder is ChatYouHolder) {
+            holder.binding.info = items[position]
+        }
     }
 }
